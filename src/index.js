970710ad07a141d786ddfcc5c5023194c2848010
gty172 <yesigatgenu@gmail.com>
2026-04-23 16:45:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  const poemBox = document.querySelector("main div");
+  let input = document.querySelector("input");
+  let topic = input.value;
 
-  poemBox.innerHTML = `
-    La tombe dit à la rose<br>
-    Des pleurs dont l'aube t'arrose
-  `;
+  let apiKey = "3fb0bbe33af251f3818a451o59ta25f7";
+  let prompt = `Write a short French-style poem about ${topic}`;
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=poetry&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
-const form = document.querySelector("form");
-form.addEventListener("submit", generatePoem);
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
