21b4d90e1ed20891791230215f28531f6a1eec4c
gty172 <yesigatgenu@gmail.com>
2026-04-23 16:56:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 function displayPoem(response) {
+  let poemElement = document.querySelector("#poem");
+
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,11 +15,14 @@ function generatePoem(event) {
   let input = document.querySelector("input");
   let topic = input.value.trim();
 
+  let poemElement = document.querySelector("#poem");
+
+  // UX improvement (loading state)
+  poemElement.innerHTML = "🤖 Generating your French poem...";
+
   let apiKey = "3fb0bbe33af251f3818a451o59ta25f7";
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=Write a short French poem about ${topic}&context=poetry&key=${apiKey}`;
-
-  document.querySelector("#poem").innerHTML = "Generating poem...";
 
   axios.get(apiURL).then(displayPoem);
 }
