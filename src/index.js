f3b75e4b159259d58db1aaf0c04a0b9e1df33fa2
gty172 <yesigatgenu@gmail.com>
2026-04-23 16:50:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 40,
+    delay: 30,
     cursor: "",
   });
 }
@@ -11,15 +11,16 @@ function generatePoem(event) {
   event.preventDefault();
 
   let input = document.querySelector("input");
-  let topic = input.value;
+  let topic = input.value.trim();
 
   let apiKey = "3fb0bbe33af251f3818a451o59ta25f7";
-  let prompt = `Write a short French-style poem about ${topic}`;
 
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=poetry&key=${apiKey}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=Write a short French poem about ${topic}&context=poetry&key=${apiKey}`;
+
+  document.querySelector("#poem").innerHTML = "Generating poem...";
 
   axios.get(apiURL).then(displayPoem);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let form = document.querySelector("#poem-generator-form");
+form.addEventListener("submit", generatePoem);
